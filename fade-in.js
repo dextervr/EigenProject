787f73d1b267bb6a506a3b54a56619033be861fd
DESKTOP-UNBSCK8\dexte <Dextervanrenswoude@gmail.com>
2022-06-01 22:54:31 +0200

    added effects when scrolling down the site

--- fade-in.js
+++ fade-in.js
@@ -1,36 +1,32 @@
-// const faders = document.querySelectorAll('.fade-in');
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
 
 
-// const appearOptions = {
-//     threshold: 1,
-//     rootMargin: "0px 0px -200px 0px"
-// };
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+};
 
-// const appearOnScroll = new IntersectionObserver
-// (function(
-//     entries,
-//     appearOnScroll
-// ) {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) {
-//             return;
-//         }else {
-//         entry.target.classList.add('appear');
-//         appearOnScroll.unobserve(entry.target);
-//         }
-//     })
-// },
-// appearOptions);
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }else {
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+        }
+    })
+},
+appearOptions);
 
-// faders.forEach(fader => {
-//     appearOnScroll.observe(fader);
-// });
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
 
-
-
-
-
-
-
-
-// still working on this
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
