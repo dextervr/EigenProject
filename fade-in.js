e48fd9ed59964a048793956b8d81ff31e0669ec1
DESKTOP-UNBSCK8\dexte <Dextervanrenswoude@gmail.com>
2022-06-01 12:41:27 +0200

    changed css

--- fade-in.js
+++ fade-in.js
@@ -1,26 +1,36 @@
-const faders =document.querySelectorAll('.fade-in');
+// const faders = document.querySelectorAll('.fade-in');
 
 
-const appearOptions = {
-    threshold: 1
-};
+// const appearOptions = {
+//     threshold: 1,
+//     rootMargin: "0px 0px -200px 0px"
+// };
 
-const appearOnScroll = new IntersectionObserver
-(function(
-    entries,
-    appearOnScroll
-) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        }else {
-        entry.target.classList.add('appear');
-        appearOnScroll.unobserve(entry.target);
-        }
-    })
-},
-appearOptions);
+// const appearOnScroll = new IntersectionObserver
+// (function(
+//     entries,
+//     appearOnScroll
+// ) {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//             return;
+//         }else {
+//         entry.target.classList.add('appear');
+//         appearOnScroll.unobserve(entry.target);
+//         }
+//     })
+// },
+// appearOptions);
 
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-});
+// faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+// });
+
+
+
+
+
+
+
+
+// still working on this
